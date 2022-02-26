5eb939f8d428949b26d6c1c6445f749ed6967fc3
Eva G Cagigas <evarteca@gmail.com>
2022-02-26 14:07:16 +0100

    probando axios

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -8,23 +8,24 @@ const BookModel = require("../models/Book.model.js")
 
 router.get("/", async (req, res, next) => {
     
-    res.render("books/search.hbs")    
+    res.render("books/book-search.hbs")    
 } )
 
 
 //! SEARCH BOOK POST ROUTE (RESULTS):
 
-router.get("/:id", async (req, res, next)=>{
-    const {id} = req.params.id
-
-    try{
-        //Buscar libro en la base de datos:
-    const bookFromDB = BookModel.findById(id)
-    //Buscar libro en la API:
-    const bookFromAPI = await axios.get(`apiUrl..${bookFromDB.apiID}`)
-    //Renderizar vista:
-    res.render("books/book-search.hbs") 
-
+router.post("/", async (req, res, next)=>{
+    const {title} = req.body.title
+    
+    try{        
+        const bookFromDB = BookModel.findById(id)
+        //Buscar libro en la API:
+        
+        //const bookFromAPI = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${bookFromDB.apiID}&key=${process.env.APIKEY}`, {
+            //params: `intitle:${req.body.title}`})
+            console.log(bookFromAPI)
+        //Renderizar vista:
+        res.render("books/book-search.hbs") 
     }
     
     catch (err) {
@@ -33,6 +34,24 @@ router.get("/:id", async (req, res, next)=>{
 })
 
 //! BOOK DETAIL GET ROUTE:
+
+//router.post("/", async (req, res, next)=>{
+    //const {id} = req.params.id
+
+    //try{
+        //Buscar libro en la base de datos:
+    //const bookFromDB = BookModel.findById(id)
+    //Buscar libro en la API:
+    //const bookFromAPI = await axios.get(`apiUrl..${bookFromDB.apiID}`)
+    //Renderizar vista:
+    //res.render("books/book-search.hbs") 
+
+    //}
+    
+    //catch (err) {
+    //    next(err)
+    //}
+//})
 
 module.exports = router
 
